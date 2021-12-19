c7d58602b9387ce3db02e981cd3a719e5b560366
danielratk <danielratk@gmail.com>
2021-12-19 02:06:29 -0300

    added object type for event and end random number

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var num = 1
 var objetos = {"Objects": []};
 var objItens = []
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
 
 function SpawnObject(nomeEvento, pos, yaws) {
 
@@ -15,15 +18,19 @@ function SpawnObject(nomeEvento, pos, yaws) {
   var z = cords[2];
   var eventspawn = "";
 
-  nomeEvento2 = nomeEvento.replace('_', '')
+  const num = Math.floor(Math.random() * 99999) + 1
 
-  let evento = ('        <event name="Vehicle'+nomeEvento2+''+num+'">\n            <nominal>1</nominal>\n            <min>1</min>\n            <max>1</max>\n            <lifetime>300</lifetime>\n            <restock>0</restock>\n            <saferadius>5</saferadius> <!-- The saferadius is the distance away from the player position that this event can spawn. -->\n            <distanceradius>5</distanceradius> <!-- The distanceradius is the minimum distance away from other similar event. -->\n            <cleanupradius>5</cleanupradius> <!-- The cleanupradius is the distance away from the player position that the event will despawn after lifetime ticks down. -->\n            <flags deletable="0" init_random="0" remove_damaged="0"/>\n            <position>fixed</position>\n            <limit>mixed</limit>\n            <!-- <limit>child</limit> -->\n            <active>1</active>\n            <children>\n                <child lootmax="0" lootmin="0" max="1" min="1" type="'+nomeEvento+'"/>\n            </children>\n        </event>\n');
+  var tipoObjeto = $('input[name="objectType"]:checked').val();
+
+  var nomeEvento2 = replaceAll(nomeEvento, "_", "");
+
+  let evento = ('        <event name="'+tipoObjeto+nomeEvento2+''+num+'">\n            <nominal>1</nominal>\n            <min>1</min>\n            <max>1</max>\n            <lifetime>300</lifetime>\n            <restock>0</restock>\n            <saferadius>5</saferadius> <!-- The saferadius is the distance away from the player position that this event can spawn. -->\n            <distanceradius>5</distanceradius> <!-- The distanceradius is the minimum distance away from other similar event. -->\n            <cleanupradius>5</cleanupradius> <!-- The cleanupradius is the distance away from the player position that the event will despawn after lifetime ticks down. -->\n            <flags deletable="0" init_random="0" remove_damaged="0"/>\n            <position>fixed</position>\n            <limit>mixed</limit>\n            <!-- <limit>child</limit> -->\n            <active>1</active>\n            <children>\n                <child lootmax="0" lootmin="0" max="1" min="1" type="'+nomeEvento+'"/>\n            </children>\n        </event>\n');
 
   a = yam;
   if (a < 0) {
     a = 360 - (a*-1);
   }
-  eventspawn = (`<event name="Vehicle${nomeEvento2}${num}">\n    <pos x="${x}" y="${y}" z="${z}" a="${a}" />\n</event>`);
+  eventspawn = (`<event name="${tipoObjeto}${nomeEvento2}${num}">\n    <pos x="${x}" y="${y}" z="${z}" a="${a}" />\n</event>`);
 
   var objeto = {
       "name": nomeEvento,
@@ -45,7 +52,6 @@ function SpawnObject(nomeEvento, pos, yaws) {
 
   $("#events").val($("#events").val() + evento );
   $("#eventspawn").val($("#eventspawn").val() + eventspawn );
-
 } 
 
 $("#enviar").click(function() {
@@ -58,7 +64,5 @@ $("#enviar").click(function() {
   }
 
   $("#jsonObject").val($("#jsonObject").val() + JSON.stringify(objetos, null, '\t'));
-
-  console.log(JSON.stringify(objetos, null, '\t'));
-
 });
+
